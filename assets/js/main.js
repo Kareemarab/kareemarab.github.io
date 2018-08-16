9eeec6580a8f309e25ea068b7d49919e78dfda03
Kareem Arab <kareem@5odbalak.com>
2018-08-16 09:23:11 -0400

    removing stack cuz wth

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,18 @@
 
 (function() {
 
+	/* start typed element */
+//http://stackoverflow.com/questions/24874797/select-div-title-text-and-make-array-with-jquery
+var subElementArray = $.map($('.sub-element'), function(el) { return $(el).text(); });
+$(".element").typed({
+		strings: subElementArray,
+		typeSpeed: 30,
+		contentType: 'html',
+		showCursor: false,
+		loop: true,
+		loopCount: true,
+});
+
 	"use strict";
 
 		// classList | (c) @remy | github.com/remy/polyfills | rem.mit-license.org
